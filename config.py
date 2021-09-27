c14c6e0546bacc84253170b98da0bd8ce504a16d
enock <enckkipronoh@gmail.com>
2021-09-27 08:09:41 +0300

    making corrections on the github repo

---
+++ config.py
@@ -0,0 +1,50 @@
+import os
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    QUOTES_API_BASE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
+    SECRET_KEY = 'password'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:enock@localhost/bloges'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://postgres:enock@localhost/bloges_test'
+
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://postgres:enock@localhost/bloges'
+
+    DEBUG = True
+
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+}
+
+
---
+++ start.sh
@@ -0,0 +1,5 @@
+# export SECRET_KEY['123!']
+export MAIL_USERNAME = 'enckkipronoh@gmail.com'
+export MAIL_PASSWORD = 'Hurtpeople1hurt'
+
+python3.8 manage.py server
